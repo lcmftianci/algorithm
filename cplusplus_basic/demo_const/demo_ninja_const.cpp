63fbe18a94c5416fee312a17cce6e737393d6688
lcmftianc <707010543@qq.com>
2019-11-10 18:30:14 +0800

    add ffmpeg library

---
+++ cplusplus_basic/demo_const/demo_ninja_const.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "demo_ninja_const.h"
+
+
+void DemoConst::ConstEat(){
+    std::cout << "Demo Const Eat" << std::endl;
+}
--- cplusplus_basic/demo_const/demo_ninja_main.cpp
+++ cplusplus_basic/demo_const/demo_ninja_main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "demo_ninja_const.h"
+
+extern "C"{
+    #include "libavcodec/avcodec.h"
+    #include "libavutil/avutil.h"
+}
 
 int main(int argc, char* argv[])
 {
     std::cout << "hello ninja hello clang" << std::endl;
+
+    auto dc = new DemoConst();
+    dc->ConstSay("const say");
+
     return 0;
 }
